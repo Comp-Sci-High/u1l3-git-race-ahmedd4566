f34a69892c2ab8515c7995d052889c6a96e4cb0c
ahmedd4566 <ahmed.daiyan26@compscihigh.org>
2024-09-18 15:16:12 +0000

    Favorite snack

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ console.log("Ahmed")
 // Stage and commit this change with an appropriate message
 // Task 2
 // Print your favorite snack
-
+console.log("Chocolate")
 // Stage and commit this change with an appropriate message
 // Task 3
 // Print your favorite artist
