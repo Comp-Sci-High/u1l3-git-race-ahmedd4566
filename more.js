6ffdb2900d74ca8e20345718700368b892854fba
ahmedd4566 <ahmed.daiyan26@compscihigh.org>
2024-09-18 15:20:54 +0000

    Added wingstop

--- more.js
+++ more.js
@@ -1,9 +1,9 @@
 // Task 4
 // Print your favorite food space
-
+console.log("Wingstop")
 // Stage and commit this change with an appropriate message
 // Task 5
 // Print your go to deli order
-
+console.log("Chopped cheese")
 // Stage and commit this change with an appropriate message
 // Go to README.md
